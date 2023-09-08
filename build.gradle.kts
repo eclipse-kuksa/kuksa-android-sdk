5fdc0d14a39b2259cfc13ae093a3ffb01e622edb
Andre Weber <andre.weber3@etas.com>
2023-09-08 14:53:54 +0200

    feature: Generate Dash License Report
    Closes: #2
    Signed-off-by: Andre Weber <andre.weber3@etas.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+import org.jetbrains.kotlin.incremental.createDirectory
+
 plugins {
     base
     detekt
@@ -6,6 +27,7 @@ plugins {
 subprojects {
     apply {
         plugin("ktlint")
+        from("$rootDir/dash.gradle.kts")
     }
     afterEvaluate {
         tasks.check {
@@ -20,5 +42,42 @@ subprojects {
             systemPropertiesMap[key.toString()] = value.toString()
         }
         systemProperties = systemPropertiesMap
+    }
+}
+
+tasks.register("mergeDashFiles") {
+    group = "oss"
+
+    dependsOn(
+        subprojects.map { subproject ->
+            subproject.tasks.getByName("createDashFile")
+        },
+    )
+
+    doLast {
+        val sortedLinesSet = sortedSetOf<String>()
+        files("build/oss").asFileTree.forEach { file ->
+            if (file.name != "dependencies.txt") return@forEach
+
+            file.useLines {
+                sortedLinesSet.addAll(it)
+            }
+        }
+
+        val folder = File("$rootDir/build/oss/all")
+        folder.createDirectory()
+
+        val file = File("$folder/all-dependencies.txt")
+        if (file.exists()) {
+            file.delete()
+        }
+        file.createNewFile()
+
+        val bufferedWriter = file.bufferedWriter()
+        bufferedWriter.use { writer ->
+            sortedLinesSet.forEach { line ->
+                writer.write(line + System.lineSeparator())
+            }
+        }
     }
 }
